8b1ae1f933cbc0aa0967a93c18de145c04964c85
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2019-09-29 07:39:03 +0100

    feat: define an interface for state services

--- abstract/service.py
+++ abstract/service.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, abstractproperty
 from abstract.singleton import Singleton
 
 
@@ -10,3 +10,38 @@ class Service(metaclass=ABCMeta):
 
 class SingletonService(metaclass=Singleton):
     pass
+
+
+class State(SingletonService, metaclass=ABCMeta):
+
+    def __init__(self, *args, **kwargs):
+        self._state = self.init_state(*args, **kwargs)
+
+    @property
+    @abstractmethod
+    def state(self):
+        """should return the state value"""
+
+    @state.setter
+    @abstractmethod
+    def state(self, value):
+        """should update the state value"""
+
+    @abstractmethod
+    def init_state(self, *args, **kwargs):
+        """should return the state initial value"""
+
+    @abstractmethod
+    def query(self, *args, **kwargs):
+        """should query the state to get specific value"""
+
+    @abstractmethod
+    def insert(self, *args, **kwargs):
+        """should query the state to get specific value"""
+
+    @abstractmethod
+    def update(self, *args, **kwargs):
+        """should query the state to get specific value"""
+
+    def reset_state(self, *args, **kwargs):
+        self.state = self.init_state(*args, **kwargs)
